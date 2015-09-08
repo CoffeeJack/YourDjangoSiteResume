0683838229f0b25ec0ea5b495bea062269be9126
Cat <coffeejack>
2015-09-07 21:56:37 -0700

    oops wrong github name

--- app/views.py
+++ app/views.py
@@ -12,7 +12,7 @@ YOUR_INFO = {
     'bio' : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id interdum tellus. Morbi ac lacus pellentesque, iaculis arcu quis, condimentum ipsum. Proin a ex ipsum. Aliquam accumsan cursus est, eu vulputate risus iaculis at. Quisque mollis rutrum sagittis. Nam cursus imperdiet lobortis. Sed dapibus cursus elit, ut gravida dui scelerisque non. Nulla eu velit mauris.',
     'email' : '', # Leave blank if you'd prefer not to share your email with other conference attendees
     'twitter_username' : 'DorkMix', # No @ symbol, just the handle.
-    'github_username' : "CoffeeJacker", 
+    'github_username' : "CoffeeJack", 
     'headshot_url' : 'https://c1.staticflickr.com/9/8119/8639028259_65436dfb4f_b.jpg', # Link to your GitHub, Twitter, or Gravatar profile image.
 }
     
